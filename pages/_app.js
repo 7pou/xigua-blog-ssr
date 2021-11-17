9d92c4f8167ab4727a4575f67f7b161939dcd1c9
zhaoyf <604264588@qq.com>
2021-11-17 11:47:58 +0800

    A Add Layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+/*
+ * @Author: zhao - 🍉
+ * @Date: 1985-10-26 16:15:00
+ * @LastEditors: zhao - 🍉
+ * @LastEditTime: 2021-11-17 10:37:41
+ * @Descripttion: 
+ */
+import '../styles/globals.scss'
+import Layout from "@/layout";
+function MyApp({ Component, pageProps, router }) {
+  const noLayoutRouteList = ['/']
+  if (noLayoutRouteList.includes(router.route) ) {
+    return <Component {...pageProps} />
+  }
+  return <Layout><Component {...pageProps} /></Layout> 
 }
+
 
 export default MyApp
