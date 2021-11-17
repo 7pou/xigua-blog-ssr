580e1f0c1da0947a6f65712ea33d904e97ad5257
zhaoyf <604264588@qq.com>
2021-11-17 11:21:46 +0800

    A  Add Layout

---
+++ layout/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from './layout.module.scss'
+import Tags from "@/containers/Tags";
+const Layout = (props) => {
+  return (
+    <div className={styles['layout']}>
+      <div className={styles['header']}></div>
+      <div className={styles['container']}>
+        <div className={styles['content']}>{props.children}</div>
+        <aside className={styles['slide-block']}>
+          <Tags />
+          <Tags />
+          <Tags />
+        </aside>
+      </div>
+     
+    </div>
+  )
+}
+
+export default Layout
