3e6e470b815f796aef2e58804d773a08e359028e
zhaoyf <604264588@qq.com>
2021-11-17 11:52:50 +0800

    A Add  base config

---
+++ common/config.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: zhao - 🍉
+ * @Date: 2021-04-15 16:55:49
+ * @LastEditors: zhao - 🍉
+ * @LastEditTime: 2021-04-15 16:55:50
+ * @Descripttion: 
+ */
---
+++ next.config.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: zhao - 🍉
+ * @Date: 2021-04-15 20:19:53
+ * @LastEditors: zhao - 🍉
+ * @LastEditTime: 2021-04-16 09:11:45
+ * @Descripttion: 
+ */
+module.exports = {
+
+}
