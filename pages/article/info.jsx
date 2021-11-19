2645e6c5ef59aee351e29ce455f69e17a53c94a6
zhaoyf <604264588@qq.com>
2021-11-19 19:22:49 +0800

    U  Add markwodn style

--- pages/article/info.jsx
+++ pages/article/info.jsx
@@ -1,24 +1,32 @@
+import React from 'react'
+import Head from "next/head";
 import { fetchArticleInfo } from '@/services/article';
 import { parseTime } from '@/utils/date';
-import React from 'react'
 import styles from "./article.module.scss";
+import { Viewer } from '@bytemd/react';
+import highlight from "@bytemd/plugin-highlight-ssr";
+import gfm from "@bytemd/plugin-gfm";
 const ArticleInfo = (props) => {
-
+  const plugins = [highlight(), gfm()]
   return (
-    <div className={styles['article-info']}>
-      <div className={styles.title}>{props.articleData.title}</div>
-      <div className={styles.time}>
-        <span>{parseTime(props.articleData.create_time)}</span>
-        <span >阅读 {props.articleData.view_count || 0}</span>
+    <div>
+      <Head>
+        <title>{props.articleData.title}</title>
+        <meta name="keywords" content="" />
+        <meta name="description" content={props.articleData.title} />
+      </Head>
+      <div className={styles['article-info']}>
+        <h1 className={styles.title}>{props.articleData.title}</h1>
+        <div className={styles.time}>
+          <span>{parseTime(props.articleData.create_time)}</span>
+          <span >阅读 {props.articleData.view_count || 0}</span>
+        </div>
+        <div className={styles.line}></div>
+        <article className={styles.content}>
+          <Viewer plugins={plugins} value={props.articleData.content}></Viewer>
+        </article>
       </div>
-      <div className={styles.line}></div>
-      <div className={styles.content}>
-        <pre style={{overflowX:'auto'}}>
-          <code>
-            {props.articleData.content}
-          </code>
-        </pre>
-      </div>
+
     </div>
   )
 }
