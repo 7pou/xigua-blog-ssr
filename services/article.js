afc7efe0d0436c399fc1662dc2f6e235e14e386a
zhaoyf <604264588@qq.com>
2021-12-23 15:03:29 +0800

    A 添加 tag API

--- services/article.js
+++ services/article.js
@@ -2,7 +2,7 @@
  * @Author: zhao - 🍉
  * @Date: 2021-04-15 16:53:58
  * @LastEditors: zhao - 🍉
- * @LastEditTime: 2021-04-15 19:56:14
+ * @LastEditTime: 2021-12-05 17:23:14
  * @Descripttion: 
  */
 import request from "../utils/request";
@@ -20,4 +20,9 @@ export function fetchArticleInfo (data) {
 
 export function fetchArticleView (data) {
   return request({ url: '/article/view', data })
+}
+
+
+export function fetchArticleTags(data) {
+  return request({ url: '/article/tags', data })
 }
