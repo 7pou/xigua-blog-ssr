719b49b980b83b952d4d21ea86dbf80cc0e71328
zhaoyf <604264588@qq.com>
2022-02-09 13:42:44 +0800

    A  添加 BAIDU_TONGJI_SRC

--- common/config.js
+++ common/config.js
@@ -2,7 +2,7 @@
  * @Author: zhao - 🍉
  * @Date: 2021-04-15 16:55:49
  * @LastEditors: zhao - 🍉
- * @LastEditTime: 2021-12-23 15:09:19
+ * @LastEditTime: 2022-02-09 12:42:14
  * @Descripttion: 
  */
 
@@ -11,3 +11,4 @@
 export const HOST = 'http://127.0.0.1:7001'
 export const REQUEST_BASE_URL = '/app'
 export const REQUEST_TIMEOUT = 15000
+export const BAIDU_TONGJI_SRC = 'https://hm.baidu.com/hm.js?6c5a79674955c233f2cd380cf960b3ed';
